3ea8dc0af13232dcefc7297ac17d59fee430674d
Ishan Bhanuka <bhanuka.ishan@gmail.com>
2022-08-01 11:41:14 +0530

    Add test runner for ocaml parser

---
+++ shared/src/test/scala/mlscript/OcamlParserTests.scala
@@ -0,0 +1,22 @@
+package mlscript
+
+import fastparse._
+import mlscript.utils._, shorthands._
+import mlscript._
+import scala.annotation.nowarn
+
+class OcamlParserTests extends org.scalatest.funsuite.AnyFunSuite {
+  import OcamlParserTests._
+
+  val fileContents = os.read(os.Path(filePath)).splitSane('\n').toList.toIndexedSeq;
+  val processedBlock = OcamlParser.addTopLevelSeparators(fileContents);
+  val fph = new FastParseHelpers(fileContents);
+  val ans =
+    parse(processedBlock.mkString, p => new OcamlParser(Origin(filePath, 0, fph)).letS(p), verboseFailures = true)
+  println(ans)
+}
+
+object OcamlParserTests {
+  val filePath =
+    "/home/twitu/Code/mlscript/shared/src/test/diff/analysis/test.ocaml"
+}
